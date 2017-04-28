2f9c1a51339178593973fba33f0863a8ed3fee06
amir-halperin <amiram.halperin@gmail.com>
2017-04-28 13:05:50 +0300

    remove not mandatory logic

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,15 @@
 import {Component} from '@angular/core';
-import {Routes, ROUTER_DIRECTIVES} from '@angular/router';
 
 @Component({
   selector: 'app',
   template: require('./app.component.html'),
   styles: [require('./app.component.css')],
-  directives: [ ROUTER_DIRECTIVES ],
-  providers: [],
 })
-@Routes([
-])
-export class AppComponent {}
+
+export class AppComponent 
+{
+  constructor()
+  {
+    console.log('app started');
+  }
+}
